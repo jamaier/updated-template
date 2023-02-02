4beaded1df93eff58504bd2160e35a5823a8bd15
Jacob Maier <jacobamaier@gmail.com>
2023-02-01 16:34:12 -0800

    Update Web-pack for images.

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,49 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ESLintPlugin = require('eslint-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  devtool: 'eval-source-map',
-  devServer: {               
-    contentBase: './dist'    
+  devtool: "eval-source-map",
+  devServer: {
+    contentBase: "./dist",
   },
   plugins: [
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'pig-dice-ref',
-      template: './src/index.html',
-      inject: 'body'
-    })
+      title: "template",
+      template: "./src/index.html",
+      inject: "body",
+    }),
   ],
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /.(gif|png|avid|jpe?g)$/,
         use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
-    ]
-  }
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "assets/images/",
+            },
+          },
+        ],
+      },
+      {
+        test: /.html$/,
+        use: ["html-loader"],
+      },
+    ],
+  },
 };
